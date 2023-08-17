c240e7a164c94a1073dec7398223ace59c7ea302
ElberthMartin <1438878@senati.pe>
2023-08-17 13:47:24 -0500

    first commit

---
+++ operador/src/main/java/com/mycompany/operador/Operador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operador;
+
+/**
+ *
+ * @author USER
+ */
+public class Operador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
