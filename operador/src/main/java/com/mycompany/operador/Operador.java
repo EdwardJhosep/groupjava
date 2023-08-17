af567cb413ff7f678f950cd8820eae9137861b1a
ElberthMartin <1438878@senati.pe>
2023-08-17 15:06:53 -0500

    Mostramos  la Ventana de Diálogo

--- operador/src/main/java/com/mycompany/operador/Operador.java
+++ operador/src/main/java/com/mycompany/operador/Operador.java
@@ -15,5 +15,17 @@ public class Operador {
 
         //declaramos y asignamos del Array de Opciones
        String[] opciones = { "Suma", "Resta", "Multiplicación", "División" };
- 
-}
+ //Mostramos  la Ventana de Diálogo usando JOptionPane.showOptionDialog():
+
+    int seleccion = JOptionPane.showOptionDialog(
+    null,                            // Componente padre (en este caso, ninguno)
+    "Seleccione una operación:",     // Mensaje a mostrar en la ventana
+    "Calculadora",                   // Título de la ventana
+    JOptionPane.DEFAULT_OPTION,      // Tipo de opción predeterminada
+    JOptionPane.QUESTION_MESSAGE,    // Tipo de mensaje (pregunta)
+    null,                            // Icono personalizado (en este caso, ninguno)
+    opciones,                        // Opciones disponibles
+    opciones[0]                      // Opción predeterminada seleccionada
+);
+
+    }
