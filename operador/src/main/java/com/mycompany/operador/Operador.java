e59777d8444a77dd3b9c83c20d41e904d1c015a6
ElberthMartin <1438878@senati.pe>
2023-08-17 15:02:09 -0500

    declaramos y asignamos del Array de Opciones

--- operador/src/main/java/com/mycompany/operador/Operador.java
+++ operador/src/main/java/com/mycompany/operador/Operador.java
@@ -13,4 +13,7 @@ public class Operador {
     // aqui declaramos el metodo main
     public static void main(String[] args) {
 
+        //declaramos y asignamos del Array de Opciones
+       String[] opciones = { "Suma", "Resta", "Multiplicación", "División" };
+ 
 }
