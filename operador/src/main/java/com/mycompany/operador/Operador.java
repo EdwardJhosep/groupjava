d7eec3430c498ce001d94805608f65b7306bd3cc
ElberthMartin <1438878@senati.pe>
2023-08-17 14:38:53 -0500

    prueba coneccion

--- operador/src/main/java/com/mycompany/operador/Operador.java
+++ operador/src/main/java/com/mycompany/operador/Operador.java
@@ -11,6 +11,6 @@ package com.mycompany.operador;
 public class Operador {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("operador");
     }
 }
